023802c655baeb63fb831dccb74b5e57ede124a1
noe adrian acuna prado <acuscorp@gmail.com>
2021-10-06 21:36:40 -0500

    Adding BinaryOperator interface

--- Client.java
+++ Client.java
@@ -1,40 +1,27 @@
+import java.util.Base64;
+
 @FunctionalInterface
 interface Square<T>{
     T calculate(T x);
 }
 
-//@FunctionalInterface
-//interface Map<K,V,U> {
- // map()
-//}
-
-class Pair<K,V>{
- private final K key;
- private final V value;
- 
- public Pair(K key, V value){
-   this.key = key;
-   this.value = value;
- }
- 
- public K getKey(){
-   return key;
- }
- public V getValue(){
-   return value;
- }
+@FunctionalInterface
+interface BinaryOperation<T>{   
+    T calculate(T x, T y);
 }
- 
 class Client{
+  
     public static void main(String args[]){
+
+       
         Integer a = 5;
         Square<Integer> square = (x)->x*x;
-        int ans = square.calculate(a);
-        System.out.println(ans);
+        int ans = square.calculate(a);     
+        System.out.println(ans);              
         
-        Square<Double> dSquare = (v)-> v*v;
+        BinaryOperation<Double> mult = (first,second)-> first*second;
         
-        double dRes = dSquare.calculate(3.6);
+        double dRes = mult.calculate(3.6,4.7);
         
         System.out.println(dRes);
     }
