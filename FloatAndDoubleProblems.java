7f88c1ca98052d2f36e25b8b12c9c2b32b1f17ad
acuscorp <acuscorp@gmail.com>
2021-10-08 23:18:01 -0500

    FIXED -- uses floating point for monetary calculation!

--- FloatAndDoubleProblems.java
+++ FloatAndDoubleProblems.java
@@ -1,18 +1,27 @@
+import java.math.BigDecimal;
+
 public class FloatAndDoubleProblems{
   public static void working(){
-    System.out.println(1.03 - 0.42);
-    System.out.println(103 - 042);
-    System.out.println(1.00 - 9 * 0.10);
-    
-    double funds = 1.00;
+    final BigDecimal TEN_CENTS = new BigDecimal(".10");
+
     int itemsBought = 0;
 
-    for (double price = 0.10; funds >= price; price += 0.10) {
-        funds -= price;
+    BigDecimal funds = new BigDecimal("1.00");
+    
+    for (BigDecimal price = TEN_CENTS;
+
+            funds.compareTo(price) >= 0;
+
+            price = price.add(TEN_CENTS)) {
+
+        funds = funds.subtract(price);
+
         itemsBought++;
     }
+    
     System.out.println(itemsBought + " items bought.");
 
-    System.out.println("Change: $" + funds);
+    System.out.println("Money left over: $" + funds);
+
   }
 } 
